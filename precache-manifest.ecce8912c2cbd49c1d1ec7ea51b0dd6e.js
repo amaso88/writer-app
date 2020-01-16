296187a3efca91bd992e8c4f71b577dbffcf752f
Armando Masó Mosqueda <amaso88@gmail.com>
2020-01-15 22:18:15 -0500

    -Uploaded static site.

---
+++ precache-manifest.ecce8912c2cbd49c1d1ec7ea51b0dd6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fef10e0d6c4039350605d17e6c27562",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39fced4f28ccfc7e83a8",
+    "url": "/static/css/2.c0f5ac91.chunk.css"
+  },
+  {
+    "revision": "49308c850e908477fd78",
+    "url": "/static/css/main.1374b118.chunk.css"
+  },
+  {
+    "revision": "39fced4f28ccfc7e83a8",
+    "url": "/static/js/2.a76aa947.chunk.js"
+  },
+  {
+    "revision": "82a1d3fe99d9f36d248eed8b0e81b9ca",
+    "url": "/static/js/2.a76aa947.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49308c850e908477fd78",
+    "url": "/static/js/main.7e073c2b.chunk.js"
+  },
+  {
+    "revision": "4206a7943424394dbbfc",
+    "url": "/static/js/runtime-main.e0339229.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
